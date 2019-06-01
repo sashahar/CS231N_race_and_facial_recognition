486800903f0d7698c8922775a0593c7a3faa492b
Sasha Harrison <sashaharrison1997@gmail.com>
2019-06-01 14:48:41 -0700

    added debugging logging

--- gender_vgg.py
+++ gender_vgg.py
@@ -35,20 +35,13 @@ def check_acc(model,data_loader):
         torch.cuda.empty_cache()
     return float(num_correct)/num_sample
 
-def plot_performance_curves(train_acc_history,val_acc_history,epoch_history):
-	plt.figure()
-	plt.plot(np.array(epoch_history),np.array(train_acc_history),label = 'Training accuracy')
-	plt.plot(np.array(epoch_history),np.array(val_acc_history),label = 'Validation accuracy')
-	plt.title('Accuracy on training and validation')
-	plt.ylabel('Accuracy')
-	plt.xlabel('Number of epochs')
-	plt.legend()
-	plt.savefig('acc_recode.png')
-
-def save_checkpoint(state,is_best,file_name = 'vgg_checkpoint_v3.pth.tar'):
+def save_checkpoint(state,is_best,file_name = 'vgg_checkpoint_v4.pth.tar'):
 	torch.save(state,file_name)
 	if is_best:
-		shutil.copyfile(file_name,'vgg_model_best_v3.pth.tar')
+        print("saving best model best model")
+		shutil.copyfile(file_name,'vgg_model_best_v4.pth.tar')
+    else:
+        print(saving checkpoint)
 
 train_transform = transforms.Compose([
 	transforms.RandomResizedCrop(224),
@@ -77,22 +70,12 @@ test_loader = torch.utils.data.DataLoader(test_data,
 NUM_CLASSES = 2
 print("Number of Training Classes: {}".format(NUM_CLASSES))
 
-
-
-# model = resnet18(pretrained=True)
-# #for param in model.parameters():
-# #   param.requires_grad = False
-
-# # Parameters of newly constructed modules have requires_grad=True by default
-# num_ftrs = model.fc.in_features
-# model.fc = nn.Linear(num_ftrs, NUM_CLASS)
-# Load the pretrained model from pytorch
 vgg16 = models.vgg16(pretrained = True)
-
+print(vgg16)
 
 # Freeze training for all layers
 for param in vgg16.features.parameters():
-    param.require_grad = False
+    param.require_grad = True
 
 num_features1 = vgg16.classifier[3].in_features
 num_features2 = vgg16.classifier[6].in_features
@@ -105,12 +88,11 @@ vgg16.classifier = nn.Sequential(*features) # Replace the model classifier
 #     nn.ReLU(),
 #     nn.Linear(hidden_layer_size, 10),)
 
-if use_gpu:
-    vgg16.cuda()
+vgg16 = vgg16.to(device)
 
 criterion = nn.CrossEntropyLoss()
-#optimizer = torch.optim.SGD(vgg16.parameters(), lr=0.001, momentum=0.9)
-optimizer = torch.optim.Adam(vgg16.parameters(), lr=0.001)
+optimizer = torch.optim.SGD(vgg16.parameters(), lr=0.001, momentum=0.9)
+# optimizer = torch.optim.Adam(vgg16.parameters(), lr=0.001)
 # Decay LR by a factor of 0.1 every 7 epochs
 exp_lr_scheduler = lr_scheduler.StepLR(optimizer, step_size=7, gamma=0.1)
 
@@ -120,54 +102,41 @@ train_acc_history = []
 val_acc_history = []
 epoch_history = []
 
-def train_model(vgg16, criterion, optimizer, scheduler, num_epochs=10):
-	best_val_acc = 0.0
+def train_model(vgg16, criterion, optimizer, scheduler, num_epochs=2):
+    best_val_acc = 0.0
+    print("Accuracy before training: ", check_acc(vgg16, test_loader))
 
-	print("Accuracy before training: ", check_acc(vgg16, test_loader))
-
-	for epoch in range(num_epochs):
-        #optimizer = torch.optim.SGD(vgg16.parameters(),lr=learning_rate,momentum=0.9)
+    for epoch in range(num_epochs):
 		print('Starting epoch %d / %d' % (epoch + 1, num_epochs))
-        #print('Learning Rate for this epoch: {}'.format(learning_rate))
 
-		vgg16.train(True)
+        for labels, race_labels, img_names, images in train_loader:
+            images = images.to(device)
+            labels = labels.to(device)
 
-        #for gender_labels, race_labels, img_names, images in train_loader:
-		for i,(labels, race_labels, img_names, images) in enumerate(train_loader):
-			images = Variable(images)
-			labels = Variable(labels)
-			if use_gpu:
-				images,labels = Variable(images.cuda()),Variable(labels.cuda())
+            optimizer.zero_grad()
+            outputs = vgg16(images)
+            loss = criterion(outputs,labels)
+            loss.backward()
+            optimizer.step()
 
-			optimizer.zero_grad()
-			pred_labels = vgg16(images)
-			loss = criterion(pred_labels,labels)
+            if (i+1) % 5 == 0:
+                print ('Epoch [%d/%d], Iter [%d/%d] Loss: %.4f'
+                    %(epoch+1, num_epochs, i+1, len(train_data)//50, loss.data))
 
-			loss.backward()
-			optimizer.step()
-			torch.cuda.empty_cache()
-
-			if (i+1) % 5 == 0:
-				print ('Epoch [%d/%d], Iter [%d/%d] Loss: %.4f'
-					%(epoch+1, num_epochs, i+1, len(train_data)//50, loss.data))
-
-		vgg16.train(False)
-		vgg16.eval()
-		if epoch % 5 ==0 or epoch == num_epochs-1:
-
-			train_acc = check_acc(vgg16,train_loader)
-			train_acc_history.append(train_acc)
+        if epoch % 5 ==0 or epoch == num_epochs-1:
+            train_acc = check_acc(vgg16,train_loader)
+            train_acc_history.append(train_acc)
             train_msg = 'Train accuracy for epoch {}: {} '.format(epoch + 1,train_acc)
-			print(train_msg)
+            print(train_msg)
             epoch_history.append(train_msg)
-
-			val_acc = check_acc(vgg16,test_loader)
+            val_acc = check_acc(vgg16,test_loader)
 			val_acc_history.append(val_acc)
 			val_msg = 'Validation accuracy for epoch {}: {} '.format(epoch + 1,val_acc)
             print(val_msg)
             epoch_history.append(val_msg)
 			#plot_performance_curves(train_acc_history,val_acc_history,epoch_history)
 			best_val_acc = max(val_acc,best_val_acc)
+            print("new best val acc: ", best_val_acc)
 
 			is_best = val_acc > best_val_acc
 			save_checkpoint(
@@ -177,4 +146,4 @@ def train_model(vgg16, criterion, optimizer, scheduler, num_epochs=10):
 				'optimizer':optimizer.state_dict()},is_best)
 	np.savetxt("training_log_vgg_v3.out", epoch_history, fmt='%s')
 
-train_model(vgg16, criterion, optimizer, exp_lr_scheduler, num_epochs=10)
+train_model(vgg16, criterion, optimizer, exp_lr_scheduler)
